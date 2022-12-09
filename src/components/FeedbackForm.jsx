717efbc3d133df2f159af36e258d91ca35dfd22c
Elybourland <JEBourland@yahoo.com>
2022-12-09 14:13:47 -0800

    rating select component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import Card from './shared/Card';
 import Button from './shared/Button';
+import RatingSelect from './RatingSelect';
 
 const FeedbackForm = () => {
   const [text, setText] = useState('');
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
 
@@ -29,7 +31,7 @@ const FeedbackForm = () => {
         <h2>
           How would you rate your service with us?
         </h2>
-        {/* @todo - rating select component */}
+        <RatingSelect select={setRating} selected={rating} />
         <div className='input-group'>
           <input 
             onChange={handleTextChange} 
