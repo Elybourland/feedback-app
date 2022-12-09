a7155192c2bc855a7e22cc41eb88ea897b13b58a
Elybourland <JEBourland@yahoo.com>
2022-12-09 14:01:28 -0800

    real time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,22 @@ import Button from './shared/Button';
 
 const FeedbackForm = () => {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
+
 
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if(text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters.')
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
     setText(e.target.value);
   }
 
@@ -23,8 +37,10 @@ const FeedbackForm = () => {
             value={text}
             placeholder='Write a review' 
           />
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>Send</Button>
         </div>
+
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
